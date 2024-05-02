a0be5fa66543646318d00ca945a5bc1dafc1077c
Learner_9ZH3Z195 <fatimareej90@gmail.com>
2024-05-02 15:22:50 -0400

    PrintingDigit

---
+++ src/com/Areej/practiceSBA/PrintingDigit.java
@@ -0,0 +1,11 @@
+package com.Areej.practiceSBA;
+
+public class PrintingDigit
+{ String num = "597";
+    public void getNumber (){
+        for ( int i = 0; i < num.length(); i++ )
+            System.out.println("Number "+(i+1) + " is " +num.charAt(i));
+
+        }
+    }
+
